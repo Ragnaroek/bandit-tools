d203593b7116fb7f524c0553d1fc2313d2746fbe
Michael Bohn <michael.bohn@freiheit.com>
2018-05-24 07:02:17 +0200

    file input test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  stateSelect(e) {
+    console.log("state selected", e.target.files);
+  }
+
+  logSelect(e) {
+    console.log("log selected", e.target.files);
+  }
+
   render() {
     return (
       <div className="App">
-        TODO :)
+        <div>
+          input state file:
+          <input name="myFile" type="file" onChange={this.stateSelect}/>
+        </div>
+        <div>
+          input log file:
+          <input name="myFile" type="file" onChange={this.logSelect}/>
+        </div>
       </div>
     );
   }
