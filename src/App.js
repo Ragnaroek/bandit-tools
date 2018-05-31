cc6f3036647c46f4834e5953812cc6e0ca1a1415
Michael Bohn <michael.bohn@freiheit.com>
2018-05-31 06:51:02 +0200

    play around with victory charts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+
+const data = [
+  {
+    "arm": "1",
+    "counts": 56
+  },
+  {
+    "arm": "2",
+    "counts": 56
+  },
+  {
+    "arm": "3",
+    "counts": 32
+  }
+]
 
 class App extends Component {
 
   stateSelect(e) {
-    console.log("state selected", e.target.files);
+    let reader = new FileReader();
+    reader.onloadend = function() {
+
+      console.log(JSON.parse(reader.result).arms);
+    };
+    console.log("file", e.target.files[0]);
+    reader.readAsText(e.target.files[0]);
   }
 
   logSelect(e) {
@@ -21,6 +43,12 @@ class App extends Component {
           input log file:
           <input name="myFile" type="file" onChange={this.logSelect}/>
         </div>
+
+        <VictoryChart domainPadding={20}>
+          <VictoryAxis label="Arms" />
+          <VictoryAxis label="Draw Count" dependentAxis={true}/>
+          <VictoryBar data={data} x="arm" y="counts"/>
+        </VictoryChart>
       </div>
     );
   }
